c9d6e61697b3c12435a5079b21b1da00226db530
Cory <corya14@gmail.com>
2017-01-22 02:08:58 -0500

    GeofenceNotify IntentService added handlers

--- app/src/main/java/com/a1403/aditumall/GeofenceNotify.java
+++ app/src/main/java/com/a1403/aditumall/GeofenceNotify.java
@@ -1,7 +1,12 @@
 package com.a1403.aditumall;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
@@ -13,8 +18,8 @@ import java.util.List;
  * Created by Cory on 1/22/2017.
  */
 
-public class GeofenceNotify extends IntentService{
-    public static final String TAG = "GeofenceService";
+public class GeofenceNotify extends IntentService {
+    public static final String TAG = "GeofenceNotify";
 
     public GeofenceNotify() {
         super(TAG);
@@ -23,8 +28,8 @@ public class GeofenceNotify extends IntentService{
     @Override
     protected void onHandleIntent(Intent intent) {
         // Can extract event and handle
-        GeofencingEvent event  = GeofencingEvent.fromIntent(intent);
-        if(event.hasError()) {
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+        if (event.hasError()) {
             // TODO: Handle error
         } else {
             int transition = event.getGeofenceTransition();
@@ -35,9 +40,44 @@ public class GeofenceNotify extends IntentService{
             // event handling
             if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
                 Log.d(TAG, "Entering geofence - " + requestId);
+                onGeofenceEnter(requestId);
             } else if (transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
                 Log.d(TAG, "Exiting geofence - " + requestId);
+                onGeofenceExit(requestId);
             }
         }
+    }
+    private void onGeofenceEnter(String geofenceId) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.map_pin)
+                        .setContentTitle(geofenceId)
+                        .setContentText("View accessibility information");
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(13, mBuilder.build());
+    }
+
+    private void onGeofenceExit(String geofenceId) {
+        //TODO: handle exit of geofence
     }
 }
